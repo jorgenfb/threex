d27088a71aca0028daca03993ae13a299b9d9be3
Jørgen Borgesen <joergen.borgesen@gmail.com>
2013-02-17 19:51:19 +0100

    No longer requires dom element to be positioned in the upper left corner of the page. Now calculates mouse position relative to dom element

--- threex.domevent.js
+++ threex.domevent.js
@@ -310,15 +310,15 @@ THREEx.DomEvent.prototype._onMouseUp	= function(event){ return this._onMouseEven
 
 THREEx.DomEvent.prototype._onMouseEvent	= function(eventName, domEvent)
 {
-	var mouseX	= +(domEvent.clientX / window.innerWidth ) * 2 - 1;
-	var mouseY	= -(domEvent.clientY / window.innerHeight) * 2 + 1;
+	var mouseX	= +((domEvent.pageX - this._domElement.offsetLeft) / window.innerWidth ) * 2 - 1;
+	var mouseY	= -((domEvent.pageY - this._domElement.offsetTop) / window.innerHeight) * 2 + 1;
 	return this._onEvent(eventName, mouseX, mouseY, domEvent);
 }
 
 THREEx.DomEvent.prototype._onMouseMove	= function(domEvent)
 {
-	var mouseX	= +(domEvent.clientX / window.innerWidth ) * 2 - 1;
-	var mouseY	= -(domEvent.clientY / window.innerHeight) * 2 + 1;
+	var mouseX	= +((domEvent.pageX - this._domElement.offsetLeft) / window.innerWidth ) * 2 - 1;
+	var mouseY	= -((domEvent.pageY - this._domElement.offsetTop) / window.innerHeight) * 2 + 1;
 	return this._onMove(mouseX, mouseY, domEvent);
 }
 
